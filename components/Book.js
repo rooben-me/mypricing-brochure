cd242fe6b08c73a1d7a456b4d0e43c87955cfd5a
rooben-me <rubankumar.nkl@gmail.com>
2022-04-14 19:30:28 +0530

    improved desing

--- components/Book.js
+++ components/Book.js
@@ -4,24 +4,42 @@ import HTMLFlipBook from "react-pageflip";
 
 const Book = () => {
   return (
-    <HTMLFlipBook autoSize={true} showCover={true} width={400} height={800}>
+    <HTMLFlipBook showCover={true} width={400} height={800}>
       <div>
-        <img src="/img/1.png" className="w-full h-full" />
+        <img
+          src="/img/1.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
       <div>
-        <img src="/img/2.png" className="w-full h-full" />
+        <img
+          src="/img/2.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
       <div>
-        <img src="/img/3.png" className="w-full h-full" />
+        <img
+          src="/img/3.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
       <div>
-        <img src="/img/4.png" className="w-full h-full" />
+        <img
+          src="/img/4.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
       <div>
-        <img src="/img/5.png" className="w-full h-full" />
+        <img
+          src="/img/5.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
       <div>
-        <img src="/img/6.png" className="w-full h-full" />
+        <img
+          src="/img/6.png"
+          className="w-full h-full border-2 border-slate-200 rounded-lg"
+        />
       </div>
     </HTMLFlipBook>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
+import Link from "next/link";
 import Book from "../components/Book";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Boostmyshop marketing web brocture</title>
-        <meta name="description" content="Boostmyshop marketing web brocture" />
+        <title>Boostmyshop marketing web brochure</title>
+        <meta name="description" content="Boostmyshop marketing web brochure" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
@@ -32,8 +33,22 @@ export default function Home() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <section className="w-full min-h-screen flex items-center justify-center">
+      <section className="w-full bg-[#F7F7FC] min-h-screen items-center justify-center flex flex-col ">
+        <Link href="https://www.boostmyshop.com/">
+          <a target="_blank">
+            <img src="/img/bms-logo.svg" className="mt-8" />
+          </a>
+        </Link>
+        <h1 className="mt-4 text-2xl font-semibold text-slate-600">
+          myPricing web brochure
+        </h1>
+        <div className="px-4 py-2 mt-8 mb-4 bg-slate-100 rounded-lg border border-slate-200">
+          <p className="text-indigo-600">
+            Click the below image or hover over the image
+          </p>
+        </div>
         <Book />
+        <div className="h-8" />
       </section>
     </div>
   );
