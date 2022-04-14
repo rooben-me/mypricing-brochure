d67f74992c157892e6f6fba51e685d57d08be630
rooben-me <rubankumar.nkl@gmail.com>
2022-04-14 19:03:40 +0530

    first commit

---
+++ components/Book.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import HTMLFlipBook from "react-pageflip";
+
+const Book = () => {
+  return (
+    <HTMLFlipBook autoSize={true} showCover={true} width={400} height={800}>
+      <div>
+        <img src="/img/1.png" className="w-full h-full" />
+      </div>
+      <div>
+        <img src="/img/2.png" className="w-full h-full" />
+      </div>
+      <div>
+        <img src="/img/3.png" className="w-full h-full" />
+      </div>
+      <div>
+        <img src="/img/4.png" className="w-full h-full" />
+      </div>
+      <div>
+        <img src="/img/5.png" className="w-full h-full" />
+      </div>
+      <div>
+        <img src="/img/6.png" className="w-full h-full" />
+      </div>
+    </HTMLFlipBook>
+  );
+};
+
+export default Book;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        1: "url('/img/1.png')",
+        2: "url('/img/2.png')",
+        3: "url('/img/3.png')",
+        4: "url('/img/4.png')",
+        5: "url('/img/5.png')",
+        6: "url('/img/6.png')",
+      },
+    },
+  },
+  plugins: [],
+};
