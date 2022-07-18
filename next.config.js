4c4dcb99930f26e0d09456e7d27f31d95286882d
rooben-me <rubankumar.nkl@gmail.com>
2022-07-18 12:19:02 +0530

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
 
 module.exports = nextConfig;
